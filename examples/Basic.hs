f38c9282591d47c56bcb5eeb6c3cb487b2edab18
Markus <mararon@chalmers.se>
2016-10-14 18:10:53 +0200

    .

--- examples/Basic.hs
+++ examples/Basic.hs
@@ -21,29 +21,34 @@ import Language.Syntactic (Internal)
 apa :: forall m .
   ( CoMonad m
   , CoType m (Expr m Int8)
-       
+  , CoType m (Expr m Word8)
        -- ugh...
   , Num (Internal (Expr m Int8))
+  , Num (Internal (Expr m Word8))
   )
   => m ()
 apa = 
-  do r <- initRef one
+  do r <- initRef (value 1 :: Expr m Int8)
+     w <- initRef (value 0 :: Expr m Word8)
+     
      setRef r (value 2)
+     
      v <- getRef r
      let x = v `plus` value 1
      setRef r x
-  where
-    one :: Expr m Int8
-    one = value 1
+  -- Soft.icompile apa
+  -- Hard.icompile apa
 
 --------------------------------------------------------------------------------
 
 bepa :: Software ()
 bepa = apa
+  -- Soft.icompile bepa
 
 --------------------------------------------------------------------------------
 
 cepa :: Hardware ()
 cepa = apa
+  -- Hard.icompile cepa
 
 --------------------------------------------------------------------------------
