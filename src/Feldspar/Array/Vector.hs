58b4016c039ccec05b28e1632eed6f1e6934c755
Markus <mararon@chalmers.se>
2017-10-01 18:32:12 +0200

    Filled in vector lib.

--- src/Feldspar/Array/Vector.hs
+++ src/Feldspar/Array/Vector.hs
@@ -5,6 +5,7 @@
 {-# language UndecidableInstances   #-}
 {-# language MultiParamTypeClasses  #-}
 {-# language FunctionalDependencies #-}
+{-# language DefaultSignatures      #-}
 
 module Feldspar.Array.Vector where
 
@@ -12,7 +13,9 @@ import Feldspar
 import Feldspar.Frontend (Arrays)
 import Feldspar.Storable
 
-import Prelude hiding (take, drop, length)
+import Control.Monad ((<=<), void)
+
+import Prelude hiding (take, drop, reverse, length, zip, zipWith, sum, min)
 
 --------------------------------------------------------------------------------
 -- * 1-dimensional vector library.
@@ -36,7 +39,7 @@ import Prelude hiding (take, drop, length)
 --------------------------------------------------------------------------------
 
 -- | A 1-dimensional vector with a concrete representation in memory
-newtype Manifest m a = M { manifest :: Array m a }
+newtype Manifest m a = M { manifest :: IArray m a }
 
 --------------------------------------------------------------------------------
 -- ** Pull vectors.
@@ -59,9 +62,17 @@ instance (Expr m ~ e) => Indexed e (Pull m a)
     type Elem (Pull m a) = a
     Pull _ ixf ! i = ixf i
 
-instance (Expr m ~ e) => Slicable e (Pull m a)
+instance ( Expr m ~ e
+         -- hmm...
+         , Cond (Expr m)
+         , Ordered (Expr m)
+         , SyntaxM' m (Expr m Length)
+         , Primitive (Expr m) Length
+         , Num (Expr m Length)
+         )
+    => Slicable e (Pull m a)
   where
-    slice from n = undefined -- todo: take n . drop from
+    slice from n = take n . drop from
 
 instance (Expr m ~ e) => Finite e (Pull m a)
   where
@@ -85,27 +96,36 @@ instance ( Indexed (Expr m) vec
 -- | Convert a 'Pully' vector to 'Pull' vector.
 toPull :: Pully m vec a => vec -> Pull m a
 toPull vec = Pull (length vec) (vec!)
-{-
+
 -- | Take the head of a vector.
-head :: Pully m vec a => vec -> a
+head :: Num (Expr m Index) => Pull m a -> a
 head = (!0)
--}
+
 -- | Drop the head of a vector.
-tail :: (Pully m vec a, Num (Expr m Length)) => vec -> Pull m a
+tail :: Num (Expr m Length) => Pull m a -> Pull m a
 tail = drop 1
 
 -- | Take the 'l' first elements of a vector.
-take :: (Pully m vec a, Ord (Expr m Length)) => Expr m Length -> vec -> Pull m a
+take :: ( Cond (Expr m)
+        , Ordered (Expr m)
+        , SyntaxM' m (Expr m Length)
+        , Primitive (Expr m) Length)
+  => Expr m Length -> Pull m a -> Pull m a
 take l vec = Pull (min (length vec) l) (vec!)
 
 -- | Drop the 'l' first elements of a vector.
-drop :: (Pully m vec a, Num (Expr m Length)) => Expr m Length -> vec -> Pull m a
+drop :: Num (Expr m Length) => Expr m Length -> Pull m a -> Pull m a
 drop l vec = Pull (length vec - l) ((vec!) . (+l))
 
-tails :: (Pully m vec a, Num (Expr m Length)) => vec -> Pull m (Pull m a)
+tails :: Num (Expr m Length) => Pull m a -> Pull m (Pull m a)
 tails vec = Pull (length vec + 1) (`drop` vec)
 
-inits :: (Pully m vec a, Num (Expr m Length), Ord (Expr m Length)) => vec -> Pull m (Pull m a)
+inits :: ( Cond (Expr m)
+         , Ordered (Expr m)
+         , Num (Expr m Length)
+         , SyntaxM' m (Expr m Length)
+         , Primitive (Expr m) Length)
+  => Pull m a -> Pull m (Pull m a)
 inits vec = Pull (length vec + 1) (`take` vec)
 
 replicate :: Expr m Length -> a -> Pull m a
@@ -114,19 +134,24 @@ replicate l = Pull l . const
 map :: Pully m vec a => (a -> b) -> vec -> Pull m b
 map f vec = Pull (length vec) (f . (vec!))
 
-zip :: Ord (Expr m Length) => (Pully m vec1 a, Pully m vec2 b) => vec1 -> vec2 -> Pull m (a, b)
+zip :: ( Cond (Expr m)
+       , Ordered (Expr m)
+       , SyntaxM' m (Expr m Length)
+       , Primitive (Expr m) Length)
+  => Pull m a -> Pull m b -> Pull m (a, b)
 zip a b = Pull (length a `min` length b) (\i -> (a!i, b!i))
 
 -- | Back-permute a 'Pull' vector using an index mapping. The supplied mapping
 --   must be a bijection when restricted to the domain of the vector. This
 --   property is not checked, so use with care.
-backPermute :: Pully m vec a
-    => (Expr m Length -> Expr m Index -> Expr m Index) -> (vec -> Pull m a)
+backPermute
+    :: (Expr m Length -> Expr m Index -> Expr m Index)
+    -> (Pull m a -> Pull m a)
 backPermute perm vec = Pull len ((vec!) . perm len)
   where
     len = length vec
 
-reverse :: Num (Expr m Index) => Pully m vec a => vec -> Pull m a
+reverse :: Num (Expr m Index) => Pull m a -> Pull m a
 reverse = backPermute $ \len i -> len-i-1
 
 (...) :: Num (Expr m Index)
@@ -136,6 +161,31 @@ reverse = backPermute $ \len i -> len-i-1
 l ... h = Pull (h-l+1) (+l)
 
 infix 3 ...
+
+zipWith :: ( Cond (Expr m)
+           , Ordered (Expr m)
+           , SyntaxM' m (Expr m Length)
+           , Primitive (Expr m) Length)
+  => (a -> b -> c) -> Pull m a -> Pull m b -> Pull m c
+zipWith f a b = fmap (uncurry f) $ zip a b
+
+fold :: (Loop (Expr m), SyntaxM m a) => (a -> a -> a) -> a -> Pull m a -> a
+fold f init vec = loop (length vec) init $ \i st -> f (vec!i) st
+
+--------------------------------------------------------------------------------
+
+sum :: (Loop (Expr m), Num a, SyntaxM m a) => Pull m a -> a
+sum = fold (+) 0
+
+scProd :: ( Loop (Expr m)
+          , Cond (Expr m)
+          , Ordered (Expr m)
+          , Num a
+          , SyntaxM m a
+          , SyntaxM' m (Expr m Length)
+          , Primitive (Expr m) Length)
+  => Pull m a -> Pull m a -> a
+scProd a b = sum (zipWith (*) a b)
 
 --------------------------------------------------------------------------------
 -- ** Push vectors.
@@ -171,14 +221,26 @@ instance (Expr m ~ e) => Finite e (Push m a)
 --------------------------------------------------------------------------------
 
 -- | Vectors that can be converted to 'Push'
-class Pushy m vec a | vec -> a
+class Pushy m vec a | vec -> a, vec -> m
   where
     -- | Convert a vector to 'Push'
     toPush :: vec -> Push m a
 
-instance Pushy m (Manifest m a) a
+instance ( MonadComp m
+         -- todo: make an instance instead
+         , Indexed (Expr m) (Manifest m a)
+         , Finite  (Expr m) (Manifest m a)
+         -- hmm...
+         , Integral (Internal (Expr m Length))
+         , Num (Expr m Length)
+         -- hmm...
+         , Elem (Manifest m a) ~ a
+         , SyntaxM' m (Expr m Length)
+         , Primitive (Expr m) Length
+         )
+    => Pushy m (Manifest m a) a
   where
-    toPush = undefined -- todo: toPush . toPull . manifest
+    toPush = toPush . toPull
 
 instance (m1 ~ m2) => Pushy m1 (Push m2 a) a
   where
@@ -266,6 +328,88 @@ instance ViewManifest m (Manifest m a) a
   where
     viewManifest = Just
 
+class ViewManifest m vec a => Manifestable m vec a | vec -> a
+  where
+    -- | Write the contents of a vector to memory and get its 'Manifest'
+    --   vector back. The supplied array may or may not be used for storage.
+    manifestArr :: (MonadComp m, SyntaxM m a)
+        => Array m a  -- ^ Where to store the vector.
+        -> vec        -- ^ Vector to store.
+        -> m (Manifest m a)
 
+    default manifestArr
+        :: ( MonadComp m
+           , SyntaxM m a
+           , Pushy m vec a
+           , Finite   (Expr m) vec
+           , Finite   (Expr m) (Array  m a)
+           , Slicable (Expr m) (IArray m a)
+           , Num (Expr m Index))
+        => Array m a -> vec -> m (Manifest m a)
+    manifestArr loc vec = do
+        dumpPush v $ \i a -> setArr loc i a
+        M <$> unsafeFreezeSlice (length vec) loc
+      where
+        v = toPush vec
+
+    -- | A version of 'manifest' that allocates a fresh array for the result.
+    manifestFresh :: SyntaxM m a => vec -> m (Manifest m a)
+
+    default manifestFresh
+        :: ( MonadComp m
+           , SyntaxM m a
+           , Finite (Expr m) vec)
+        => vec
+        -> m (Manifest m a)
+    manifestFresh vec = do
+        loc <- newArr $ length vec
+        manifestArr loc vec
+
+    -- | A version of 'manifest' that only stores the vector to the given array.
+    manifestStore :: SyntaxM m a => Array m a -> vec -> m ()
+
+    default manifestStore
+        :: ( MonadComp m
+           , SyntaxM m a
+           , Pushy m vec a
+           -- hmm...
+           , Finite   (Expr m) (Array m a)
+           , Slicable (Expr m) (IArray m a)
+           , Num (Expr m Length)
+           )
+        => Array m a
+        -> vec
+        -> m ()
+    manifestStore loc = void . manifestArr loc . toPush
+
+instance ( MonadComp m
+         , SyntaxM m a
+         , Finite (Expr m) (IArray m a))
+    => Manifestable m (Manifest m a) a
+  where
+    manifestArr _     = return
+    manifestFresh     = return
+    manifestStore loc = copyArr loc <=< unsafeThawArr . manifest
+
+instance ( MonadComp m
+         , SyntaxM   m a
+         , Finite   (Expr m) (Array m a)
+         , Slicable (Expr m) (IArray m a)
+         -- hmm...
+         , Integral (Internal (Expr m Length))
+         , Num (Expr m Length)
+         -- hmm...
+         , SyntaxM' m (Expr m Length)
+         , Primitive (Expr m) Length
+         )
+    => Manifestable m (Pull m a) a
+
+instance ( MonadComp m
+         , SyntaxM m a
+         , Finite   (Expr m) (Array m a)
+         , Slicable (Expr m) (IArray m a)
+         , Num (Expr m Length)
+         )
+    => Manifestable m (Push m a) a
 
 --------------------------------------------------------------------------------
